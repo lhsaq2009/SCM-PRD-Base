f304f3396408c27580a91995076967cf48688450
haisen <lhsaq2009@163.com>
2025-05-28 11:24:37 +0800

    Auto commit for version 1.0.15

--- src/LNConnection.php
+++ src/LNConnection.php
@@ -15,7 +15,7 @@ class LNConnection extends Connection
      */
     public function createCommand($sql = null, $params = [])
     {
-        if ($sql !== null) {
+        if ($sql !== null && $sql && stripos($sql, 'UUID') !== false) {
             $sql = str_ireplace('UUID()', 'TIME_ID()', $sql);
             $sql = str_ireplace('UUID_SHORT()', 'TIME_ID()', $sql);
         }
